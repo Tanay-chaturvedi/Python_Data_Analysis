64149b6cb4b30d6082ba4b2448e29122b49bad09
Tanay Chaturvedi <tanaychaturvedi126@gmail.com>
2026-07-20 19:31:48 +0530

    basics of python

---
+++ basics/day1.py
@@ -0,0 +1,18 @@
+'''print("Hello")
+name = "Tanay"
+age = 22
+cgpa = 8.8
+placed = False'''
+
+'''print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(placed))'''
+name = "Tanay"
+age = 22
+
+print(name, age)
+name = "Tanay"
+age = 22
+
+print(f"My name is {name} and I am {age} years old.")
